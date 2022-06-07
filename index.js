7c1589e7d6a082d806d02e8c3ed916036040a4c6
eduardoaguilar96 <eduaguilar96@gmail.com>
2022-06-07 19:49:18 +0200

    proyecto backend

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+var cors = require("cors");
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo!')
 })
 
 app.get('/prueba', (req, res) => {
-    res.send('Hola Mundo2!')
+    res.send('TEXTO DE PRUEBA')
   })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
